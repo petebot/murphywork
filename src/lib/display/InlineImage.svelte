aca53267d1dc5f2bf8c59423c885549698228245
Pete Nawara <pete@petenawara.com>
2024-06-19 16:53:29 -0400

    feat(body): add inline image capabilities to contentblock

--- src/lib/display/InlineImage.svelte
+++ src/lib/display/InlineImage.svelte
@@ -4,6 +4,7 @@
 
   console.log(portableText.value);
   let imageUrl = "";
+  let alignment = "";
 
   if (
     portableText.value &&
@@ -11,16 +12,33 @@
     portableText.value.asset._ref
   ) {
     imageUrl = urlFor(portableText.value).url();
+    alignment = portableText.value.alignment || "left";
   }
 </script>
 
 {#if imageUrl}
-  <img src={imageUrl} alt={portableText.value.alt || ""} />
+  <img src={imageUrl} alt={portableText.value.alt || ""} class={alignment} />
 {/if}
 
 <style>
   img {
-    max-width: 100%;
-    display: inline;
+    max-width: 50vw;
+  }
+  img.left {
+    float: left;
+    margin-right: 1em;
+  }
+  img.right {
+    float: right;
+    margin-left: 1em;
+  }
+  img.full-width {
+    width: 100%;
+    display: block;
+  }
+  @media (max-width: 700px) {
+    img {
+      max-width: 100%;
+    }
   }
 </style>
